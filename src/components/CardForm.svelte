e85dffb5ea6ce5ad25843a34b1af4333ff7612d9
Danny Fong <danny.fong.dev@outlook.com>
2022-11-16 21:29:10 -0800

    Form validation and CSS finishes

--- src/components/CardForm.svelte
+++ src/components/CardForm.svelte
@@ -15,15 +15,121 @@
   $: $cardStore.cvc = cvc;
 
   const handleBlur = () => {
-    cNumber = format(cNumber);
+    if (cNumber !== undefined) {
+      cNumber = format(cNumber);
+    }
   };
   function format(s) {
+    // add spaces for readability
     return s.toString().replace(/\d{4}(?=.)/g, "$& ");
   }
 
-  const handleConfirm = () => {
-    dispatch("confirm", "success");
+  const handleFocus = () => {
+    // take away spaces
+    try {
+      cNumber = cNumber.replace(/ /g, "");
+    } catch {}
+  };
+
+  // Error Handling
+  let isErrorName = false;
+  let msgErrorName = "";
+
+  let isErrorNum = false;
+  let msgErrorNum = "";
+
+  let isErrorMonth = false;
+  let isErrorYear = false;
+  let msgErrorMonthYear = "";
+
+  let isErrorCvc = false;
+  let msgErrorCvc = "";
+
+  // only allow numbers and spaces
+  const regex = new RegExp("^[0-9 ]*$");
+  function IsValidNum(s) {
+    return regex.test(s);
   }
+
+  const IsValidData = () => {
+    let valid = true;
+    // validate name
+    if (cName === undefined) {
+      isErrorName = true;
+      msgErrorName = "Can't be blank";
+      valid = false;
+    } else if (cName.length < 6) {
+      isErrorName = true;
+      msgErrorName = "Must be at least six characters";
+      valid = false;
+    } else {
+      isErrorName = false;
+      msgErrorName = "";
+    }
+    // validate credit card number
+    if (!IsValidNum(cNumber)) {
+      isErrorNum = true;
+      msgErrorNum = "Wrong format, numbers only";
+      valid = false;
+    } else if (cNumber.length != 19) {
+      isErrorNum = true;
+      msgErrorNum = "Wrong format, less than 16 digits";
+      valid = false;
+    } else {
+      isErrorNum = false;
+      msgErrorNum = "";
+    }
+    // validate expiration month
+    if (expMonth === undefined) {
+      isErrorMonth = true;
+      msgErrorMonthYear = "Can't be blank";
+      valid = false;
+    } else if (expMonth.length < 2) {
+      isErrorMonth = true;
+      msgErrorMonthYear = "Must be two digits";
+      valid = false;
+    } else {
+      isErrorMonth = false;
+      msgErrorMonthYear = "";
+    }
+    // validate expiration year
+    if (expYear === undefined) {
+      isErrorYear = true;
+      msgErrorMonthYear = "Can't be blank";
+      valid = false;
+    } else if (expYear.length < 2) {
+      isErrorYear = true;
+      msgErrorMonthYear = "Must be two digits";
+      valid = false;
+    } else {
+      isErrorYear = false;
+      msgErrorMonthYear = "";
+    }
+    // valiate cvc
+    if (cvc === undefined) {
+      isErrorCvc = true;
+      msgErrorCvc = "Can't be blank";
+      valid = false;
+    } else if (cvc.length === 0) {
+      isErrorCvc = true;
+      msgErrorCvc = "Can't be blank";
+      valid = false;
+    } else if (cvc.length < 3) {
+      isErrorCvc = true;
+      msgErrorCvc = "Must be three digits";
+      valid = false;
+    } else {
+      isErrorCvc = false;
+      msgErrorCvc = "";
+    }
+    return valid;
+  };
+
+  const handleConfirm = () => {
+    if (IsValidData()) {
+      dispatch("confirm", "success");
+    }
+  };
 </script>
 
 <div class="container">
@@ -35,38 +141,64 @@
         id="c-name"
         placeholder="e.g. Jane Appleseed"
         bind:value={cName}
+        class:outline-error={isErrorName}
       />
+      {#if isErrorName}<div class="is-error">
+          {msgErrorName}
+        </div>{/if}
     </div>
     <div class="input-group">
       <label for="c-number">Card Number</label>
       <input
         on:blur={handleBlur}
+        on:focus={handleFocus}
         type="text"
         id="c-number"
         placeholder="e.g. 1234 5678 9123 0000"
         bind:value={cNumber}
+        maxlength="16"
+        class:outline-error={isErrorNum}
       />
+      {#if isErrorNum}<div class="is-error">
+          {msgErrorNum}
+        </div>{/if}
     </div>
     <div class="input-group">
-      <div class="flex-group">
-        <label for="exp-month" class="expire-padding-right"
-          >Exp. Date (MM/YY)</label
-        ><label for="cvc">CVC</label>
-      </div>
       <div class="grid-group">
+        <label for="exp-month" class="two-column">Exp. Date (MM/YY)</label
+        ><label for="cvc">CVC</label>
         <input
           type="text"
           id="exp-month"
           placeholder="MM"
           bind:value={expMonth}
+          maxlength="2"
+          class:outline-error={isErrorMonth}
         />
         <input
           type="text"
           id="exp-year"
           placeholder="YY"
           bind:value={expYear}
+          maxlength="2"
+          class:outline-error={isErrorYear}
         />
-        <input type="text" id="cvc" placeholder="e.g. 123" bind:value={cvc} />
+        <input
+          type="text"
+          id="cvc"
+          placeholder="e.g. 123"
+          bind:value={cvc}
+          maxlength="3"
+          class:outline-error={isErrorCvc}
+        />
+        {#if isErrorMonth || isErrorYear || isErrorCvc}
+          <div class="two-column is-error">
+            {msgErrorMonthYear}
+          </div>
+          <div class="is-error">
+            {msgErrorCvc}
+          </div>
+        {/if}
       </div>
     </div>
     <button on:click|preventDefault={handleConfirm}>Confirm</button>
@@ -86,36 +218,62 @@
   }
   input {
     width: 100%;
-    color: #aaa; /* fix color */
-    /* outline-style: solid; */
-    outline-color: #aaa;
+    color: var(--neutral-color-4);
+    border-style: solid;
+    border-color: var(--neutral-color-2); 
     border-radius: 10px;
     padding: 0.5em 0.9em;
-    margin-bottom: 1rem;
+    margin-bottom: 0;
+  }
+  input::placeholder {
+    color: var(--neutral-color-2);
+  }
+  input:focus {
+    outline-style: solid;
+    outline-color: var(--secondary-color);
+    /* border-image-source: linear-gradient(
+      hsl(249, 99%, 64%),
+      hsl(278, 94%, 30%)
+    );
+    border-width: 1px;
+    border-image-slice: 1;
+    border-radius: 10px; */
   }
   label {
     text-transform: uppercase;
     font-size: 0.7rem;
     letter-spacing: 0.1rem;
     margin-bottom: 0.5em;
+    margin-top: 1rem;
+    color: var(--neutral-color-4);
   }
   button {
     width: 100%;
     border-radius: 10px;
     color: white;
-    background-color: purple; /* fix color */
+    background-color: var(--neutral-color-4);
     padding: 0.7rem;
+    margin-top: 1.6rem;
   }
-  .flex-group {
-    display: flex;
+  button:hover {
+    background: var(--secondary-color);
   }
   .grid-group {
     display: grid;
     grid-template-columns: 1fr 1fr 2fr;
-    gap: 0.5rem;
+    column-gap: 0.5rem;
   }
-  .expire-padding-right {
-    padding-right: 1.6rem;
+  .two-column {
+    grid-column: 1 / 3;
+  }
+  .is-error {
+    color: var(--error-color);
+    font-size: 0.7rem;
+    padding-top: 0.3rem;
+  }
+  .outline-error {
+    outline-style: solid;
+    outline-color: var(--error-color);
   }
   @media (min-width: 800px) {
     form {
--- src/components/CompletedMessage.svelte
+++ src/components/CompletedMessage.svelte
@@ -9,32 +9,39 @@
 </div>
 
 <style>
-	.container {
-		max-width: 600px;
-		text-align: center;
-		padding: 0 25px;
-		margin: auto;
-	}
-	img {
-		margin-bottom: 40px;
-	}
-	h2 {
-		margin-bottom: 20px;
-		text-transform: uppercase;
-		letter-spacing: 0.2rem;
-	}
-	p {
-		margin-bottom: 40px;
-	}
-	button {
-		width: 100%;
-		padding: 0.7rem;
-		border-radius: 10px;
-	}
-	@media (min-width: 800px){
-		.container {
-			padding: 320px 25px 0 25px;
-			width: 430px;
-		}
-	}
+  .container {
+    max-width: 600px;
+    text-align: center;
+    padding: 0 25px;
+    margin: auto;
+  }
+  img {
+    margin-bottom: 40px;
+  }
+  h2 {
+    margin-bottom: 20px;
+    text-transform: uppercase;
+    letter-spacing: 0.2rem;
+    color: var(--neutral-color-4);
+  }
+  p {
+    margin-bottom: 40px;
+    color: var(--neutral-color-3);
+  }
+  button {
+    width: 100%;
+    padding: 0.7rem;
+    border-radius: 10px;
+    background: var(--neutral-color-4);
+    color: var(--neutral-color-1);
+  }
+  button:hover {
+    background: var(--secondary-color);
+  }
+  @media (min-width: 800px) {
+    .container {
+      padding: 320px 25px 0 25px;
+      width: 430px;
+    }
+  }
 </style>
